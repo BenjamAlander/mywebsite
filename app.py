99a2bea36def37fbd6989b85a7ddc8d6a76ef2c1
Benjam Alander <benjamalander@kamk.fi>
2023-01-18 18:58:15 +0200

    rebrand

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
-from flask import Flask
-from home import home1
-
+from flask import Flask, render_template
 app = Flask(__name__, template_folder='template')
-app.register_blueprint(home1, url_prefix="/")
 
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+    
+@app.route('/socials')
+def socials():
+    return render_template('socials.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=80)
+    app.run(debug=True)
